5a0ca94015755603d0c2d21ea1ac781c7148faf5
Ossama Qureshi <ossamasq1@outlook.com>
2022-11-21 15:39:35 -0500

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -25,10 +26,34 @@ const ReservationScreen = () => {
   };
 
   const handleReservation = () => {
+    const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString("en-US")}`;
+    Alert.alert(
+      "Begin Search?",
+      message,
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            console.log("Reservation Search Canceled");
+            resetForm();
+          },
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            presentLocalNotification(date.toLocaleDateString("en-US"));
+            resetForm();
+          },
+        },
+      ],
+      { cancelable: false }
+    );
     console.log("campers:", campers);
     console.log("hikeIn:", hikeIn);
     console.log("date:", date);
-    setShowModal(!showModal);
   };
 
   const resetForm = () => {
@@ -36,6 +61,34 @@ const ReservationScreen = () => {
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
+  };
+
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${reservationDate} requested`,
+        },
+        trigger: null,
+      });
+    };
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   };
 
   return (
@@ -85,21 +138,7 @@ const ReservationScreen = () => {
         )}
         <View style={styles.formRow}>
           <Button
-            onPress={() =>
-              Alert.alert(
-                "Begin Search?",
-                `Number of Campers: ${campers}                                                                         Hike-In? ${hikeIn}                                                                                                               Date: ${date}`,
-                [
-                  {
-                    text: "Cancel",
-                    onPress: () => resetForm(),
-                    style: "cancel",
-                  },
-                  { text: "OK", onPress: () => resetForm() },
-                ],
-                { cancelable: false }
-              )
-            }
+            onPress={() => handleReservation()}
             title="Search Availability"
             color="#5637DD"
             accessibilityLabel="Tap me to search for available campsites to reserve"
@@ -124,22 +163,6 @@ const styles = StyleSheet.create({
   },
   formItem: {
     flex: 1,
-  },
-  modal: {
-    justifyContent: "center",
-    margin: 20,
-  },
-  modalTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    backgroundColor: "#5637DD",
-    textAlign: "center",
-    color: "#fff",
-    marginBottom: 20,
-  },
-  modalText: {
-    fontSize: 18,
-    margin: 10,
   },
 });
 
